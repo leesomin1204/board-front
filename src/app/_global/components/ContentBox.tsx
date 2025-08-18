a898d9c1943737afc458eaad23afcbe5ebe010e5
leesomin1204 <sm1157@naver.com>
2025-08-18 11:05:35 +0900

    .

---
+++ src/app/_global/components/ContentBox.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const ContentBox = ({ children, className, width }) => {
+  return (
+  <section className={'layout-width ' + className} width={width}>
+        {children}
+    </section>
+    )
+}
+
+const StyledContentBox = styled<any>(ContentBox)`
+  padding: 50px;
+  ${({ width }) =>
+    width &&
+    css`
+      width: ${width}px;
+    `}
+`
+
+export default React.memo(StyledContentBox)
